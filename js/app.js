d59614132dad4fdec8e2574260b9630a71b8d56c
Kevin Johnson <kevin.johnson711@gmail.com>
2019-02-21 21:13:25 -0500

    Made arrow keys functional

--- js/app.js
+++ js/app.js
@@ -6,7 +6,8 @@ var todoList = {
       todoName: todoName,
       completed:false,
       children: [],
-      id: util.uuid(),
+       id: util.uuid(),
+      collapsed: false,
     })
     return array[indexToInsertIn].id
   },
@@ -16,30 +17,47 @@ var todoList = {
   deleteTodo: function(indexOfTodo, array){
     array.splice(indexOfTodo,1);
   },
-  toggleTodo: function(todo){
-    todo.completed = !todo.completed
+  toggleTodo: function(todo, toggleFlag){
+    //reverses the boolean value of completed unless a true toggleFlag is passed in
+    if(todo.completed === false || toggleFlag === true){
+      todo.completed = true;
+    }else{
+      todo.completed = false;
+    }
+    //recurses to toggle nested Todos
+    for(var i=0; i< todo.children.length;i++){
+      this.toggleTodo(todo.children[i], toggleFlag);
+    }
   },
   toggleAll: function(){
-    //checks if any of the todos are false
-    var aTodoIsFalse = this.todos.find(function(value){
-      return value.completed === false
-    });
+    //creates a toggleFlag that is true only if at least one todo is incomplete
+    var toggleFlag = this.checkTodos(this.todos);
     //if atleast one todo is false, set all of the todos to true
-    if(aTodoIsFalse){
-      todoList.todos.forEach(function(value){
-        value.completed = true;
-      });
-    }
-    else{
-      todoList.todos.forEach(function(value){
-        value.completed = false;
-      });
+    for(var i=0; i < this.todos.length; i++){
+      this.toggleTodo(this.todos[i], toggleFlag);
     }
   },
-  clearCompleted: function(){
-    for(var i=this.todos.length - 1; i >= 0; i--){
-      if(this.todos[i].completed === true){
-        todoList.deleteTodo(i);
+  //check Todos checks if all todos are completed. If they are all Completed
+  // it returns false
+  checkTodos: function(array){
+    var toggleFlag = false
+    //if any todos are false toggle Flag is returned as true.
+    for(var i=0; i< array.length; i++){
+      if(array[i].completed === false) return toggleFlag = true;
+      //recurses to check nested Todos.
+      if(array[i].children.length > 0){
+        this.checkTodos(array[i].children)
+      }
+    }
+    return toggleFlag;
+  },
+  clearCompleted: function(array){
+    for(var i=array.length - 1; i >= 0; i--){
+      if(array[i].completed === true){
+        todoList.deleteTodo(i, array);
+      }
+      if(array[i] !== undefined && array[i].children.length > 0){
+        this.clearCompleted(array[i].children)
       }
     }
   },
@@ -58,7 +76,7 @@ var handlers = {
     view.render();
   },
   clearCompleted: function(){
-    todoList.clearCompleted();
+    todoList.clearCompleted(todoList.todos);
     view.render();
   },
   clearAll: function(){
@@ -81,32 +99,43 @@ var view = {
       noTodosP.textContent = "Looks like there are no todos";
       ul.appendChild(noTodosP);
     }
-      this.renderLi(todoList.todos, ul);
+      this.renderList(todoList.todos, ul);
     //saves todos
     util.store('todoListData', todoList.todos)
   },
-  renderLi: function(array, ulToAppendTo){
+  renderList: function(array, ulToAppendTo){
     for(var i=0; i<array.length; i++){
       this.createTodoLi(array[i],i,array,ulToAppendTo)
       if(array[i].children.length >0){
         var nestedUl = document.createElement('ul');
         ulToAppendTo.appendChild(nestedUl);
-        this.renderLi(array[i].children, nestedUl);
+        this.renderList(array[i].children, nestedUl);
       }
     }
   },
   createTodoLi: function(todo,indexOfTodo, array, ulToAppendTo){
+    var div = document.createElement('div')
+    div.className = 'li-div'
     var todoLi = document.createElement("li")
+    var id = todo.id;
+    todoLi.id =id;
+    var todoTextP = this.createTodoP(todo.todoName, id);
     if(todo.completed === true){
       todoLi.className += 'completed';
     }
 
     var deleteButton = this.createDeleteButton(indexOfTodo, array);
     var toggleButton = this.createToggleCompletedButton(todo);
-    var editInput = this.createEditInput(todo, indexOfTodo, array);
+    var editInput = this.createEditInput(todo, indexOfTodo, array, id);
     var addSubTodoButton = this.createAddSubTodoButton(todo.children);
 
-    ulToAppendTo.appendChild(todoLi);
+    // if(todo.children.length > 0){
+    //     var collpaseButton = this.createCollapseButton(todo.collapsed);
+    //     todoLi.appendChild(collpaseButton);
+    //   }
+    ulToAppendTo.appendChild(div);
+    div.appendChild(todoLi);
+    todoLi.appendChild(todoTextP);
     todoLi.appendChild(editInput);
     todoLi.appendChild(deleteButton);
     todoLi.appendChild(toggleButton);
@@ -114,31 +143,47 @@ var view = {
 
 //when todoTextP is double clicked you can edit todo
   },
-  createEditInput: function(todo, indexOfTodo, array){
+  createTodoP: function(text, id){
+    var p = document.createElement('p');
+    p.textContent = text || 'New Todo';
+    p.id = id + 'p'
+    //when clicked hide p element and make edit input visible
+    p.addEventListener('click', function(){
+      var p = document.getElementById(id + 'p');
+      var editInput = document.getElementById(id + "editInput");
+      //makes editInput visible and p invisible
+      editInput.style.display= 'block';
+      p.style.display = 'none';
+      editInput.focus();
+    })
+
+    return p;
+  },
+  createEditInput: function(todo, indexOfTodo, array, id){
     var editInput = document.createElement('input');
     editInput.value = todo.todoName;
-    editInput.id= todo.id;
+    editInput.id= id+"editInput";
+    editInput.style.display = 'none';
 
     //makes changes and hides todo Input when enter is pressed
     editInput.addEventListener('keyup', function(e){
         todoList.editTodo(todo, editInput.value);
     })
-    //if enter is pressed add a new todo right under current todo
+    //if enter is pressed rerender
     editInput.addEventListener('keypress',function(e){
       if(e.key === "Enter"){
-        var todoId = todoList.addTodo('', array, indexOfTodo + 1)
         view.render();
-        var todoElement = document.getElementById(todoId);
-        todoElement.focus();
       }
     })
-
+    editInput.addEventListener('blur',function(e){
+        view.render();
+    })
     return editInput;
   },
   createDeleteButton: function(indexOfTodo, array){
     var deleteButton = document.createElement('button');
-    deleteButton.textContent = "deleteTodo";
-    deleteButton.className += 'delete-button'
+    deleteButton.textContent = "Delete Todo";
+    deleteButton.className = 'delete-button'
 
     deleteButton.onclick = function(){
         todoList.deleteTodo(indexOfTodo, array);
@@ -165,12 +210,25 @@ var view = {
     addSubTodoButton.onclick = function(){
       var subtodoId = todoList.addTodo('New SubTodo',array, array.length);
       view.render();
-
-      var subTodoElement = document.getElementById(subtodoId);
-      subTodoElement.focus();
+      var subTodoP = document.getElementById(subtodoId + "p")
+      subTodoP.click();
     }
 
     return addSubTodoButton;
+  },
+  createCollapseButton: function(toggledFlag,todo){
+    var collapseButton = document.createElement('button');
+    collapseButton.className = 'collapse-button';
+    //if not toggled toggle button shows '(0)'
+    if(toggledFlag === false){
+      collapseButton.textContent = '( - )'
+    }else{
+      collapseButton.textContent = '( + )'
+    }
+    collapseButton.onlick = function(event,todo){
+      todo.collapsed = !todo.collapsed;
+    }
+    return collapseButton;
   },
 }
 var util = {
@@ -198,11 +256,65 @@ var util = {
 			return uuid;
 		},
 }
+//when enter is pressed on new todo input, a new todo is created
 document.getElementById("add-todo-input").addEventListener('keypress', function(e){
   if(e.key === "Enter"){
     handlers.addTodo();
   }
 })
+//makes up and down arrows functional. It should change focus of todo based on which key is pressed
+document.onkeydown = function(e){
+  addTodoInput = document.getElementById('add-todo-input')
+  currentFocusedElement = document.activeElement;
+  //If down arrow is pressed
+  if(e.code === "ArrowDown" || e.code === "ArrowUp"){
+    //if focused on add Todo Input and down key is pressed, focus on first todo
+    if(currentFocusedElement == addTodoInput ){
+      firstTodoId = todoList.todos[0].id;
+      var firstTodoP = document.getElementById(firstTodoId + 'p');
+      return firstTodoP.click();
+    }
+    //if focused on a todo input
+    if(currentFocusedElement != document.querySelector('body')){
+      //getsId of Todo from input. slice removes editInput part of id
+      var currentTodoId = currentFocusedElement.id.slice(0,-9)
+      var todoLiArray = document.getElementsByTagName("li");
+      for(var i=0; i < todoLiArray.length; i++){
+        if(todoLiArray[i].id === currentTodoId){
+          var arrayPosOfCurrentLi = i;
+        }
+      }
+      //if arrowDown is pressed, next li to focus on is below the current one
+      if(e.code === "ArrowDown"){
+        var arrayPosOfNextLi = arrayPosOfCurrentLi + 1;
+      }else{
+        //else focus on li above the current one
+        var arrayPosOfNextLi = arrayPosOfCurrentLi - 1;
+      }
+      //if next li doesnt exist, make the next li to focus on the first one
+      //or last one if arrow down is pressed
+      if(todoLiArray[arrayPosOfNextLi] === undefined){
+        if(e.code === "ArrowDown"){
+          var idOfNextTodo = todoLiArray[0].id
+        }else{
+          var idOfNextTodo = todoLiArray[todoLiArray.length - 1].id
+        }
+      }else{
+        //else get id of the next li
+        var idOfNextTodo = todoLiArray[arrayPosOfNextLi].id;
+      }
+      //moves cursor to next Todo
+      pElementOfNextTodo = document.getElementById(idOfNextTodo + 'p');
+      view.render();
+      return  pElementOfNextTodo.click();
+    }
+    //if user is not focused on anything when arrow key is pressed. It focuses on add todo input
+    else{
+      addTodoInput.focus();
+    }
+  }
+
+}
 //loads todos from last session and renders todo List
 todoList.todos = util.store('todoListData')
 view.render(document.getElementById('add-todo-input'));
